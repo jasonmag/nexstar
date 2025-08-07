6315ea0ae0f9920833f484bd69b31654c5cae12c
jasonmag <jasonmaglangit@gmail.com>
2025-08-08 00:25:43 +0800

    updated admin with theme

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,9 +15,9 @@ module Admin
     def create
       @user = User.new(user_params)
       if @user.save
-        redirect_to admin_users_path, notice: "User Created."
+        redirect_to admin_users_path, notice: "User created successfully."
       else
-        render :new
+        render :new, status: :unprocessable_entity
       end
     end
 
@@ -28,16 +28,19 @@ module Admin
     def update
       @user = User.find(params[:id])
       if @user.update(user_params)
-        redirect_to admin_users_path, notice: "User updated."
+        redirect_to admin_user_path(@user), notice: "User updated successfully."
       else
-        render :edit
+        render :edit, status: :unprocessable_entity
       end
     end
 
     def destroy
       @user = User.find(params[:id])
-      @user.destroy
-      redirect_to admin_users_path, notice: "User deleted."
+      if @user.destroy
+        redirect_to admin_users_path, notice: "User was deleted successfully."
+      else
+        redirect_to admin_users_path, alert: "Could not delete user."
+      end
     end
 
     private
