61ddb5cf6086d1cfc7fa75dee0042798806976a5
jasonmag <jasonmaglangit@gmail.com>
2025-11-09 20:17:03 +0800

    updated project partial with image url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_03_143703) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_09_114842) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_03_143703) do
     t.date "released_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
