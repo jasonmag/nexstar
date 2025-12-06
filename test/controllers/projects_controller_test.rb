652395697f2298f95856b1b4ead5c836137cc9d3
jasonmag <jasonmaglangit@gmail.com>
2025-12-07 03:21:29 +0800

    Fix projects controller tests to use correct route helpers

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -2,12 +2,13 @@ require "test_helper"
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get projects_index_url
+    get projects_url
     assert_response :success
   end
 
   test "should get show" do
-    get projects_show_url
+    project = projects(:one)
+    get project_url(project)
     assert_response :success
   end
 end
